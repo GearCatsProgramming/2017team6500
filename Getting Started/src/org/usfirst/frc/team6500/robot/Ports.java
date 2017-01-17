2b0550d4b675bcf810b4e9a3d1266c61f653d551
gearcats6500 <gearcats6500@gmail.com>
2017-01-17 16:03:00 -0500

    Initial Commit

---
+++ Getting Started/src/org/usfirst/frc/team6500/robot/Ports.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team6500.robot;
+
+public abstract class Ports{
+	public static int leftfront = 0;
+	public static int leftrear = 1;
+	public static int rightfront = 2;
+	public static int rightrear = 3;
+	public static int joystickid = 0;
+}
